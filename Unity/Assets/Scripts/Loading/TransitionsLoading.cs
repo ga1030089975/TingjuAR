4092fd53b92603f464f9f099e1fb5e166b7df470
deli <1073255569@qq.com>
2021-12-06 15:33:26 +0800

    Loading场景搭建

---
+++ Unity/Assets/Scripts/Loading/TransitionsLoading.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TransitionsLoading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SceneManager>().LoadScene();
+    }
+
+}

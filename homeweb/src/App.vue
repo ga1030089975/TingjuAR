9f0a43caf4e0da3d98d5f03071750e60c7276f22
jiaylee <jiaylee@163.com>
2021-12-07 16:45:58 +0800

    框架初版

--- homeweb/src/App.vue
+++ homeweb/src/App.vue
@@ -1,36 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: {   
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ homeweb/src/views/Status.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-container style="height: 100%">
+      <el-main>
+        <h1>This is personal center page</h1>
+      </el-main>
+      <el-footer><common-footer> </common-footer></el-footer>
+    </el-container>
+</template>
+<script>
+import CommonFooter from '../components/CommonFooter.vue'
+export default {
+  name:"Home",
+  components: { CommonFooter },    
+};
+</script>
+<style scoped>
+.el-footer{
+  background-color: #425c5a;
+  color: #ffcea2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.el-main {
+  padding-top: 0%;
+}
+</style>

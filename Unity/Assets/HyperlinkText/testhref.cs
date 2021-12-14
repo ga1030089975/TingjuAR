098ef2496a449f2ffedece56c764289fd4cc1be2
zcf-ustc <zhangchaofan@mail.ustc.edu.cn>
2021-12-14 15:37:55 +0800

    Merge branch 'dev' of github.com:ga1030089975/TingjuAR into dev

---
+++ Unity/Assets/HyperlinkText/testhref.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testhref : MonoBehaviour {
+
+    private HyperlinkText mHyperlinkText;
+    public GameObject userGo;
+    public GameObject privacyGo;
+     public GameObject UserAndPrivacy;
+
+    void Awake()
+    {
+        mHyperlinkText = GetComponent<HyperlinkText>();
+    }
+
+
+    protected  void OnEnable()
+    {
+        mHyperlinkText.onHrefClick.AddListener(OnHyperlinkTextInfo);
+    }
+
+    protected  void OnDisable()
+    {
+        mHyperlinkText.onHrefClick.RemoveListener(OnHyperlinkTextInfo);
+    }
+    /// <summary>
+    /// 当前点击超链接回调
+    /// </summary>
+    /// <param name="info">回调信息</param>
+    private void OnHyperlinkTextInfo(string info)
+    {
+        Debug.Log("超链接信息：" + info);
+        switch (info)
+        {
+            case "yhxy":
+                UserAndPrivacy.SetActive(true);
+                userGo.SetActive(true);
+                privacyGo.SetActive(false);
+                break;
+            case "yszc":
+                UserAndPrivacy.SetActive(true);
+                userGo.SetActive(false);
+                privacyGo.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
